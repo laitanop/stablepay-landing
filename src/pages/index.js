c9614741c7a2e92e7b0d3658559dbac3ba0eb2e1
Pamela Laitano <laitanop@yahoo.com>
2020-08-30 13:42:42 -0600

    contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,11 +133,11 @@ export default () => (
           </Package>
         </IconContext.Provider>
       </Packages>
-      {/* <Contact
+      <Contact
         id="contact"
         title="Contact Startup today and see how we can help your business grow"
         subtitle="Every second counts when you're looking to get your new business started. Drop Startup a message and one of our representatives will be in contact"
-      /> */}
+      />
     </Layout>
   </>
 )
